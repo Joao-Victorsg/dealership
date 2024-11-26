b47537156426259fbc8871d0ce5ac72ee78997cf
João Victor <joaovsg@id.uff.br>
2024-11-26 02:18:59 -0300

    infra: Including the parameters to the database infra

---
+++ infra-databases/infra/parameters.tf
@@ -0,0 +1,21 @@
+resource "aws_ssm_parameter" "database_username"{
+  name = "/delearship/api/client/database/username"
+  description = "Parameter that refers the username from the client database"
+  type = "SecureString"
+  value = var.client_database_username
+
+  tags = {
+    context = "delearship-client-api"
+  }
+}
+
+resource "aws_ssm_parameter" "database_password" {
+  name = "/dealership/api/client/database/password"
+  description = "Parameter that contains the ARN of the secret that contains the database password"
+  type = "String"
+  value = aws_secretsmanager_secret.api_client_password.arn
+
+  tags = {
+    context = "delearship-client-api"
+  }
+}
