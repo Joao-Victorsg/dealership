f6c65b0fbaef68532c2cad5ab45ca013099e0da5
João Victor <joaovsg@id.uff.br>
2025-05-03 02:06:58 -0300

    feature: Updating main.tf and dockerfile lambda-fetch-client-info

--- lambda-fetch-client-info/infra/main.tf
+++ lambda-fetch-client-info/infra/main.tf
@@ -1,12 +1,12 @@
 resource "aws_lambda_function" "fetch_client_info" {
   function_name = "fetch-client-Info"
   role          = aws_iam_role.lambda_exec_role.arn
-  image_uri = "${data.aws_ecr_repository.lambda_fetch_client_info.repository_url}:1.0"
+  image_uri = "${data.aws_ecr_repository.lambda_fetch_client_info.repository_url}:2.0"
   package_type = "Image"
 
   environment {
     variables = {
-      client_API_URL = "${data.aws_apigatewayv2_api.api_gateway_url.api_endpoint}:8085/v1/dealership/clients"
+      client_API_URL = "http://b6c8c8b9.execute-api.localhost.localstack.cloud:8085/v1/dealership/clients"
     }
   }
 }
