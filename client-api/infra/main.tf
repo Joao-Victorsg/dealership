90d6c01c3017666288c21e1814c84dbed3757b51
João Victor <joaovsg@id.uff.br>
2025-05-17 00:16:34 -0300

    infra: Updating dockerfile and main.tf to get the new container version.

--- client-api/infra/main.tf
+++ client-api/infra/main.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "api-client" {
   container_definitions = <<DEFINITION
 [
   {
-    "image": "joaovictorsg/client-api:1.3",
+    "image": "joaovictorsg/client-api:1.4",
     "cpu": 1024,
     "memory": 2048,
     "name": "client-api",
