524c59195b98c0ad8e5e38bf54c59c7c7bf5fcea
João Victor <joaovsg@id.uff.br>
2025-05-18 00:15:22 -0300

    infra: Changing the load balancer to be a NLB and adjusting the API Gateway to that

--- infra-vpc/apigtw.tf
+++ infra-vpc/apigtw.tf
@@ -5,6 +5,10 @@ resource "aws_apigatewayv2_api" "api" {
   name          = "dealership-api-gateway"
   protocol_type = "HTTP"
   body = file("${path.module}/open_api_specification/gateway_specification.yaml")
+
+  tags = {
+    "_custom_id_" = "ff50bf03"
+  }
 }
 
 #2: VPC Link
@@ -15,23 +19,62 @@ resource "aws_apigatewayv2_vpc_link" "vpc_link" {
 }
 
 #3: API Integration
-resource "aws_apigatewayv2_integration" "api_integration" {
+resource "aws_apigatewayv2_integration" "clients_integration" {
   api_id             = aws_apigatewayv2_api.api.id
   integration_type   = "HTTP_PROXY"
   connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
   connection_type    = "VPC_LINK"
-  description        = "VPC integration"
+  description        = "Clients API integration"
   integration_method = "ANY"
-  integration_uri    = aws_lb_listener.dealership-api.arn
+  integration_uri    = "${aws_lb.dealership_lb.dns_name}:8085"
+
+  depends_on         = [aws_lb.dealership_lb]
+}
+
+resource "aws_apigatewayv2_integration" "cars_integration" {
+  api_id             = aws_apigatewayv2_api.api.id
+  integration_type   = "HTTP_PROXY"
+  connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
+  connection_type    = "VPC_LINK"
+  description        = "Cars API integration"
+  integration_method = "ANY"
+  integration_uri    = "http://${aws_lb.dealership_lb.dns_name}:8087"
+  #integration_uri    = "http://host.docker.internal:8087"
+
+
+  depends_on         = [aws_lb.dealership_lb]
+}
+
+resource "aws_apigatewayv2_integration" "sales_integration" {
+  api_id             = aws_apigatewayv2_api.api.id
+  integration_type   = "HTTP_PROXY"
+  connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
+  connection_type    = "VPC_LINK"
+  description        = "Sales API integration"
+  integration_method = "ANY"
+  integration_uri    = "${aws_lb.dealership_lb.dns_name}:8086"
+
   depends_on         = [aws_lb.dealership_lb]
 }
 
 #4: APIGW Route
-/*resource "aws_apigatewayv2_route" "default_route" {
+resource "aws_apigatewayv2_route" "clients_routes" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
-}*/
+  route_key = "ANY /v1/dealership/clients/{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.clients_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "cars_routes" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /v1/dealership/cars/{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.cars_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "sales_routes" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /v1/dealership/sales/{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.sales_integration.id}"
+}
 
 #5: APIGW Stage
 resource "aws_apigatewayv2_stage" "default_stage" {
--- infra-vpc/lb.tf
+++ infra-vpc/lb.tf
@@ -2,24 +2,9 @@
 resource "aws_lb" "dealership_lb" {
   name            = "api-dealership"
   subnets         = aws_subnet.private.*.id
-  security_groups = [aws_security_group.security_group_lb.id]
+  load_balancer_type = "network"
   internal = true
   tags = {
     "Name" = "api-dealership"
-  }
-}
-
-resource "aws_lb_listener" "dealership-api" {
-  load_balancer_arn = aws_lb.dealership_lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404 Not Found"
-      status_code  = "404"
-    }
   }
 }
